c4127c12c288009f263383c10aa8c48c570df6eb
VannWasHere <ateen1203@gmail.com>
2023-12-01 23:34:39 +0700

    Add Models

--- api/app.py
+++ api/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pickle
 
 api = Flask(__name__)
 CORS(api)
 
 api.config['stored_data'] = None
 
+# model = pickle.load(open('nn_model.pkl', 'rb'))
+X_scaler = pickle.load(open('X_scaler.pkl', 'rb'))
+y_scaler = pickle.load(open('y_scaler.pkl', 'rb'))
+
 @api.route('/check', methods=['POST'])
 def handle_post():
-    received_data = request.get_json()
-    api.config['stored_data'] = received_data
+    stored_data_dict = api.config.get('stored_data', {})
+    stored_data_list = list(stored_data_dict.values())
     
-    return jsonify({'data': received_data})
+    print(stored_data_list)
+
 
 @api.route('/sendData', methods=['GET'])
 def handle_get():
