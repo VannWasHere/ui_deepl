b473aa0dba6d43d29a3365dd368fc72345700126
VannWasHere <ateen1203@gmail.com>
2023-12-01 13:36:18 +0700

    Add Input Component and Disable Proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 const App = () => {
   return (
-    <h1 className="">Coba Tailwind</h1>
+    <div className="p-16">
+      <Input label={"Tahun Kendaraan"}/>
+    </div>
+  );
+}
+
+const Input = ({label, name}) => {
+  return (
+    <>
+      <label htmlFor={name} className="text-xl">{label}</label>
+        <input type="number" 
+      className="w-full p-3.5 mt-2 outline-none bg-indigo-500 focus:outline-none text-white placeholder:text-white rounded-md"
+      name={name}
+      placeholder={label}/>
+    </>
   );
 }
 
