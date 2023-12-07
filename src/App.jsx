b37ae75c1e50571b12a6ee8012fefb6534e3df80
VannWasHere <ateen1203@gmail.com>
2023-12-07 23:28:32 +0700

    Set Form Validation and Add display

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,16 @@ const App = () => {
     tahunKendaraan: 0,
     kilometer: 1,
     merk: 1,
+    lokasi_item: 'Kota Jakarta Barat',
+    kapasitasMesin_item: '110cc',
+    kilometer_item: '0 - 1000 KM',
+    merk_item: 'Yamaha',
   });
 
-  const onHandleSubmitted = (name, value) => {
-    setUserInput((prevState) => ({...prevState, [name]: value}))
+  const onHandleSubmitted = (name, value, item) => {
+    // Add receive data item
+    setUserInput((prevState) => ({...prevState, [name]: value, [name + '_item']: item}));
+    console.log(userInput);
   }
 
   const submitForm = async () => {
@@ -43,11 +49,11 @@ const App = () => {
     }
   }
 
-  // Unfinished Method
-  const showInputData = async () => {
-
+  if(userInput.tahunKendaraan < 2000 || userInput.tahunKendaraan > 2023) {
+    setUserInput((prevState) => ({...prevState, tahunKendaraan: 2000}))
   }
 
+  // Sending and Request Data using Axios
   const sendData = async (formData) => {
     try {
       await axios.post('http://localhost:5000/check', {
@@ -90,15 +96,20 @@ const App = () => {
               <Select label={"Lokasi"} name={"lokasi"} 
               data={['Kota Jakarta Barat', 'Kab. Tangerang', 'Kota Jakarta Selatan', 'Kota Bandung', 'Kota Jakarta Timur', 'Kab. Bogor', 'Kota Jakarta Utara', 'Kota Medan',
               'Kota Tangerang', 'Kab. Bandung', 'Kota Batam', 'Kota Tangerang Selatan', 'Kota Bekasi', 'Kota Depok', 'Kota Surabaya', 'Kab. Deli Serdang']} 
-              onChange={(value) => onHandleSubmitted("lokasi", value)}/>
+              onChange={(value,item) => onHandleSubmitted("lokasi", value, item)}/>
 
               <SelectCC label={"Kapasitas Mesin"} name={"kapasitasMesin"}
               data={['110cc', '115cc', '125cc', '150cc', '155cc', '160cc', '175cc', '220cc', '250cc', '600cc', '900cc']}
-              onChange={(value) => onHandleSubmitted("kapasitasMesin", value)}/>
+              onChange={(value, item) => onHandleSubmitted("kapasitasMesin", value, item)}/>
 
               <Input label={"Tahun Kendaraan"} name={"tahunKendaraan"} onChange={(value) => onHandleSubmitted("tahunKendaraan", value)}/>
-              <Select label={"Kilometer"} name={"kilometer"} data={['0 - 1000 KM', '1000 - 5000 KM', '5000 - 10000 KM', '10000 - 20000 KM', '30000 KM']} onChange={(value) => onHandleSubmitted("kilometer", value)}/>
-              <Select label={"Merk"} name={"merk"} data={['Yamaha', 'Honda', 'Kawasaki', 'Suzuki']} onChange={(value) => onHandleSubmitted("merk", value)}/>
+
+              <Select label={"Kilometer"} name={"kilometer"} data={['0 - 1000 KM', '1000 - 5000 KM', '5000 - 10000 KM', '10000 - 20000 KM', '30000 KM']} 
+              onChange={(value, item) => onHandleSubmitted("kilometer", value, item)} />
+              
+              <Select label={"Merk"} name={"merk"} data={['Yamaha', 'Honda', 'Kawasaki', 'Suzuki']} 
+              onChange={(value, item) => onHandleSubmitted("merk", value, item)}/>
+
               <button onClick={(e) => submitForm(e)} className="mt-12 bg-indigo-800 w-full p-4 rounded-lg text-white font-bold tracking-wider uppercase">Predict</button>
             </div>
             <div className="w-full h-full mt-9 p-5 flex justify-center">
@@ -116,23 +127,23 @@ const App = () => {
                     <tbody className="w-full">
                       <tr>
                         <td className="text-lg text-indigo-900">Lokasi</td>
-                        <td className="text-lg text-indigo-900 text-center w-1/3">Test</td>
+                        <td className="text-lg text-indigo-900 text-center w-1/3">{userInput.lokasi_item}</td>
                       </tr>
                       <tr>
                         <td className="text-lg text-indigo-900">Kapasitas Mesin</td>
-                        <td className="text-lg text-indigo-900 text-center w-1/3">Test</td>
+                        <td className="text-lg text-indigo-900 text-center w-1/3">{userInput.kapasitasMesin_item}</td>
                       </tr>
                       <tr>
                         <td className="text-lg text-indigo-900">Tahun Kendaraan</td>
-                        <td className="text-lg text-indigo-900 text-center w-1/3">Test</td>
+                        <td className="text-lg text-indigo-900 text-center w-1/3">{userInput.tahunKendaraan}</td>
                       </tr>
                       <tr>
                         <td className="text-lg text-indigo-900">Kilometer</td>
-                        <td className="text-lg text-indigo-900 text-center w-1/3">Test</td>
+                        <td className="text-lg text-indigo-900 text-center w-1/3">{userInput.kilometer_item}</td>
                       </tr>
                       <tr>
                         <td className="text-lg text-indigo-900">Merk</td>
-                        <td className="text-lg text-indigo-900 text-center w-1/3">Test</td>
+                        <td className="text-lg text-indigo-900 text-center w-1/3">{userInput.merk_item}</td>
                       </tr>
                       <tr>
                         <td className="text-lg text-indigo-900 border-t-2">Hasil Prediksi Harga</td>
@@ -165,15 +176,21 @@ const Input = ({label, name, onChange}) => {
 }
 
 const Select = ({label, name, data, onChange}) => {
+  const handleSelectChange = (e) => {
+    const selectedValue = e.target.value;
+    const selectedItem = e.target.options[e.target.selectedIndex].getAttribute('data-item');
+
+    onChange(selectedValue, selectedItem);
+  };
   return (
     <div className="mt-9">
       <label htmlFor={name} className="text-xl text-indigo-700">{label}</label> <br />
       <select name={name} id={name} 
-      onChange={(e) => onChange(e.target.value)}
+      onChange={handleSelectChange }
       className="w-full p-3.5 mt-2 outline-4 outline-white bg-indigo-500 text-white text-lg placeholder:text-white rounded-md shadow-lg shadow-indigo-400 focus:outline-none">
       
         {data.map((item, key) => (
-          <option key={key} value={key + 1} 
+          <option key={key} value={key + 1} data-item={item}
           className="">{item}</option>
         ))}
       
@@ -183,15 +200,21 @@ const Select = ({label, name, data, onChange}) => {
 }
 
 const SelectCC = ({label, name, data, onChange}) => {
+  const handleSelectChange = (e) => {
+    const selectedValue = e.target.value;
+    const selectedItem = e.target.options[e.target.selectedIndex].getAttribute('data-item');
+
+    onChange(selectedValue, selectedItem);
+  };
   return (
     <div className="mt-9">
       <label htmlFor={name} className="text-xl text-indigo-700">{label}</label> <br />
       <select name={name} id={name} 
-      onChange={(e) => onChange(e.target.value)}
+      onChange={handleSelectChange}
       className="w-full p-3.5 mt-2 outline-4 outline-white bg-indigo-500 text-white text-lg placeholder:text-white rounded-md shadow-lg shadow-indigo-400 focus:outline-none">
       
         {data.map((item, key) => (
-          <option key={key} value={item} 
+          <option key={key} value={item} data-item={item}
           className="">{item}</option>
         ))}
       
