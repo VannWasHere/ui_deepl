7c445b64f1500489a2428c1f700903c9f551a0f0
VannWasHere <ateen1203@gmail.com>
2023-12-08 00:00:06 +0700

    Update UI

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,7 +1,7 @@
 const Gallery = () => {
     return (
         <>
-            <h2 className="text-2xl font-bold text-center">Supported Brand</h2>
+            <h2 className="text-2xl font-bold text-center">Available Brand</h2>
             <div className="w-full flex gap-5 justify-center">
                 <img className="w-32" src={"./src/images/logo_honda.svg"} alt="honda" />
                 <img className="w-40" src={"./src/images/logo_kawasaki.svg"} alt="honda" />
