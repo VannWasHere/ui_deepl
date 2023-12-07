ba57b3e5a0df6c5d5ec94217e399407955780b75
VannWasHere <ateen1203@gmail.com>
2023-12-07 23:41:57 +0700

    Change component's directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import Loading from "./images/Components/Loading";
+import Loading from "./Components/Loading";
 
 const App = () => {
 
